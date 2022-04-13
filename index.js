7dddd4ddcc96340198babf022ab331b43af0cc54
Daniel Phillips <dan.phillips100@gmail.com>
2022-04-13 12:23:41 +0100

    Exercise 3.7 and 3.8 completed

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+
+morgan.token('person', (request, response) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 let persons = [
   { 
@@ -105,12 +112,10 @@ app.post('/api/persons', (request, response) => {
   persons = persons.concat(person);
 
   response.json(person);
-
 })
-
-
 
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
+
