365fcf234165a97a927c276dde8cbbb85eb9401c
Daniel Phillips <dan.phillips100@gmail.com>
2022-04-21 13:35:00 +0100

    Exercises 3.13 and 3.14 complete

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors')
+// const mongoose = require('mongoose')
+const Person = require('./models/person');
+const { response } = require('express');
 
 app.use(express.json());
 app.use(cors())
@@ -12,52 +16,104 @@ morgan.token('person', (request, response) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  },
-  { 
-    "id": 5,
-    "name": "This is coming from dev", 
-    "number": "94785239"
-  }
-]
+// let persons = [
+//   { 
+//     "id": 1,
+//     "name": "Arto Hellas", 
+//     "number": "040-123456"
+//   },
+//   { 
+//     "id": 2,
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523"
+//   },
+//   { 
+//     "id": 3,
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345"
+//   },
+//   { 
+//     "id": 4,
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122"
+//   },
+//   { 
+//     "id": 5,
+//     "name": "This is coming from dev", 
+//     "number": "94785239"
+//   }
+// ]
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// const password = process.argv[2]
+// const url =
+//   `mongodb+srv://danPhone:${password}@cluster0.fuzt0.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+// mongoose.connect(url)
+
+// const personSchema = new mongoose.Schema({
+//   name: String,
+//   number: String,
+// })
+
+// personSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject._id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+
+
+// const Person = mongoose.model('Person', personSchema)
+
+
+
+
+
+
+
+
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook</h1>');
 })
 
+// app.get('/api/persons', (request, response) => {
+//   response.json(persons);
+// })
+
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons);
+  })
 })
 
 // get individual entry from persons
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
+// app.get('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id);
+//   const person = persons.find(person => person.id === id);
 
-  if (person) {
+//   if (person) {
+//     response.json(person)
+//   } else {
+//     response.status(404).end();
+//   }
+// })
+
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end();
-  }
+  })
 })
 
 // get number of entries in persons
@@ -82,42 +138,56 @@ const generateId = () => {
     : 0;
   return maxId + 1;
 }
+//// post using local DB
+// app.post('/api/persons', (request, response) => {
+//   const body = request.body;
+//   // check if name is empty
+//   if (!body.name) {
+//     return response.status(400).json({
+//       error: 'Name is missing'
+//     })
+//   }
+//   // check if number is empty
+//   if (!body.number) {
+//     return response.status(400).json({
+//       error: 'Number is missing'
+//     })
+//   }
+//   // check if name is unique
+//   const duplicateName = persons.find(person => person.name === body.name);
+//   if (duplicateName) {
+//     return response.status(400).json({
+//       error: 'Name already exists in phonebook'
+//     })
+//   }
 
+//   const person = {
+//     name: body.name,
+//     number: body.number,
+//     id: generateId(),
+//   }
+
+//   persons = persons.concat(person);
+
+//   response.json(person);
+// })
+
+// Post using Mongo
 app.post('/api/persons', (request, response) => {
-  const body = request.body;
-  // check if name is empty
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'Name is missing'
-    })
-  }
-  // check if number is empty
-  if (!body.number) {
-    return response.status(400).json({
-      error: 'Number is missing'
-    })
-  }
-  // check if name is unique
-  const duplicateName = persons.find(person => person.name === body.name);
-  if (duplicateName) {
-    return response.status(400).json({
-      error: 'Name already exists in phonebook'
-    })
-  }
+  const body = request.body
 
-  const person = {
+    const person = new Person ({
     name: body.name,
-    number: body.number,
-    id: generateId(),
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
-
--- mongo.js
+++ mongo.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose')
 
-// if (process.argv.length < 3) {
-//   console.log('Please provide the password as an argument: node mongo.js <password>')
-//   process.exit(1)
-// }
-
 const password = process.argv[2]
 const personName = process.argv[3]
 const personNumber = process.argv[4]
@@ -14,29 +9,30 @@ const url =
 
 mongoose.connect(url)
 
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-  })
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
 
-  const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema)
 
-  const person = new Person({
-    name: personName,
-    number: personNumber,
-  })
 
-  if(!personName || !personNumber) {
-    Person.find({}).then(result => {
-      result.forEach(person => {
-        console.log(person)
-      })
-      mongoose.connection.close()
-    }) 
-  } else {
-    person.save().then(result => {
-      console.log(`Added ${personName} number ${personNumber} to phonebook`)
-      mongoose.connection.close()
+const person = new Person({
+  name: personName,
+  number: personNumber,
+})
+
+if(!personName || !personNumber) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
     })
-  }
+    mongoose.connection.close()
+  }) 
+} else {
+  person.save().then(result => {
+    console.log(`Added ${personName} number ${personNumber} to phonebook`)
+    mongoose.connection.close()
+  })
+}
 
